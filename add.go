5da5309b481a68b87da70ad47e313f0292a44600
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-02-08 23:46:48 -0300

    Add godoc to add function.

--- add.go
+++ add.go
@@ -1,5 +1,9 @@
 package add
 
+// This function takes two integers
+// and returns the sum of them as an integer.
+// For more information regarding additions,
+// visit https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
